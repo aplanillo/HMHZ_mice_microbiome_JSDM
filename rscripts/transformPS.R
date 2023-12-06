763afd5ac4dbfaddd36efb0b8911b3f247eecc64
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2023-12-06 10:27:39 +0100

    A README draft for collaborative work

---
+++ rscripts/transformPS.R
@@ -0,0 +1,8 @@
+## loading packages
+source("rscripts/source_pkgs.R")
+
+
+## just a placholder
+relAbunPS <- function(PS) {
+    message("just a placeholder, this could calculate relative abundance")
+}
