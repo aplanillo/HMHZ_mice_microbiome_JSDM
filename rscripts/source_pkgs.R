b43f36206c9fccbfc5fa4ba6806f3253b373afb0
aplanillo <50693698+aplanillo@users.noreply.github.com>
2023-12-11 10:59:06 +0100

    Update source_pkgs.R
    Included code to install phyloseq

--- rscripts/source_pkgs.R
+++ rscripts/source_pkgs.R
@@ -12,3 +12,11 @@ for (i in 1:length(mylibraries)) {
   if(mylibraries[i] %in% rownames(installed.packages()) == FALSE) {install.packages(mylibraries[i])}
 }
 lapply(mylibraries, require, character.only = TRUE)
+
+
+## Installation of Phyloseq
+
+# if(!requireNamespace("BiocManager")){
+#   install.packages("BiocManager")
+# }
+# BiocManager::install("phyloseq", force = TRUE)
