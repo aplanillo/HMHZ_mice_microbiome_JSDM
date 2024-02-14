7f033b001e091f5a2576bd230f8cbe1304fe69d0
aplanillo <50693698+aplanillo@users.noreply.github.com>
2024-02-14 08:59:53 +0100

    Update source_pkgs.R

--- rscripts/source_pkgs.R
+++ rscripts/source_pkgs.R
@@ -6,7 +6,7 @@
 ####--------------------------------------------####
 
 mylibraries <- c("dplyr", "ggplot2", "phyloseq", "magrittr", "sf", "terra", 
-                 "tidyr", "units", "stringi", "tmap")
+                 "tidyr", "units", "stringi", "tmap", "tibble")
 
 for (i in 1:length(mylibraries)) {
   if(mylibraries[i] %in% rownames(installed.packages()) == FALSE) {install.packages(mylibraries[i])}
